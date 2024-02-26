ce8bce53fb8f87b7326972271ef9fb6f0725578d
Subhadip Maity <subhadipmaity09@gmail.com>
2024-02-26 10:15:13 +0530

    Basic Programs

--- BasicsOfDSA/src/feb22/Q15_InverseOfNumber.java
+++ BasicsOfDSA/src/feb22/Q15_InverseOfNumber.java
@@ -1,5 +1,33 @@
+// video no. 23 & 24
+// You are given a number following certain constraints.
+// The key constraint is if the number is 5 digits long, it'll contain all the digits from 1 to 5 without missing any and without repeating any. e.g. 23415 is a 5 digit long number containing all digits from 1 to 5 without missing and repeating any digit from 1 to 5.Take a look at few other valid numbers - 624135, 81456273 etc.Here are a few invalid numbers - 139, 7421357 etc.
+// The inverse of a number is defined as the number created by interchanging the face value and index of digits of number.e.g. for 426135 (reading from right to left, 5 is in place 1, 3 is in place 2, 1 is in place 3, 6 is in place 4, 2 is in place 5 and 4 is in place 6), the inverse will be 416253 (reading from right to left, 3 is in place 1, 5 is in place 2,2 is in place 3, 6 is in place 4, 1 is in place 5 and 4 is in place 6) More examples - inverse of 2134 is 1243 and inverse of 24153 is 24153    
+// Take as input number "n", assume that the number will follow constraints.
+// Print it's inverse.
+
 package feb22;
 
-public class Q15_InverseOfNumber {
+import java.util.*;
 
+public class Q15_InverseOfNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int inv = 0;
+		int orgPos = 1;
+		while(n != 0)
+		{
+			int orgDig = n % 10;
+			int invPos = orgDig;
+			int invDig = orgPos;
+			
+			// make change to inv using invPos & invDig
+			inv = inv + invDig*(int)Math.pow(10, invPos-1);
+			
+			n = n/10;
+			orgPos++;
+		}
+		System.out.println(inv);
+	}
 }
