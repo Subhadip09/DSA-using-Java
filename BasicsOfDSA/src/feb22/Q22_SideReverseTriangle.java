1b737737f67be102ac37f95df0931a1c524d6b75
Subhadip Maity <subhadipmaity09@gmail.com>
2024-03-12 19:14:00 +0530

    pattern 2

--- BasicsOfDSA/src/feb22/Q22_SideReverseTriangle.java
+++ BasicsOfDSA/src/feb22/Q22_SideReverseTriangle.java
@@ -4,21 +4,21 @@
 // 2.you've to create a pattern of * and separated by tab shown in o/p format
 
 // 5
-// *
-// * *
-// * * *
-// * * * *
 // * * * * *
+// * * * *
+// * * *
+// * * 
+// *
 package feb22;
 
 import java.util.*;
 
-public class Q21_SideTrianglePattern {
+public class Q22_SideReverseTriangle {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
-		for(int i = 1; i<=n; i++)
+		for(int i = n; i>=1; i--)
 		{
 			for(int j = 1; j<=i; j++)
 			{
